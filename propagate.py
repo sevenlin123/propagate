0e1948bd62c67186107f6377f660f4df9c4c0848
Edward Lin <edward.lin@me.com>
2018-07-10 23:58:57 -0700

    v0.4.2: bug fixed

--- propagate.py
+++ propagate.py
@@ -1,6 +1,6 @@
 ##########################################################################
 #
-# propagate.py, version 0.3
+# propagate.py, version 0.4.2
 #
 # calculate equatorial sky positions for given Keplerian orbits and epochs     
 #  
@@ -8,7 +8,8 @@
 # Edward Lin hsingwel@umich.edu
 # v0.2: improve efficiency
 # v0.3: work for Python3
-#
+# v0.4: optional heliocentric elements input 
+# v0.4.2: heliocentric elements work correctly
 ##########################################################################
 
 from __future__ import division
@@ -17,7 +18,7 @@ from skyfield.api import Topos, Loader
 from scipy.optimize import newton
 
 load = Loader('./Skyfield-Data', expire=False)
-planets = load('de423.bsp')
+planets = load('de430t.bsp')
     
 class propagate:
     """
@@ -25,18 +26,29 @@ class propagate:
     Units: au, radian, JD
       
     """
-    def __init__(self, a, e, i, arg, node, M0, epoch, obs_date):
-        self.a = a
-        self.e = e
-        self.i = i
-        self.arg = arg
-        self.node = node
-        self.M0 = M0
+    def __init__(self, a, e, i, arg, node, M0, epoch, obs_date, helio=False):
+        self.u_bary = 2.9630927492415936E-04 # standard gravitational parameter  
+        self.u_helio = 2.9591220828559093E-04 
+        #self.u = 0.00029591220650534517
+        self.epsilon =  23.43929 * np.pi/180. # obliquity
+        #self.epsilon = 23.4368883003 * np.pi/180.
+        if helio:
+            self.u  = self.u_helio
+            self.a, self.e, self.i, self.arg, self.node, self.M0 =  self.helio_to_bary(a, e, i, arg, node, M0, epoch)
+        else:
+            self.u = self.u_bary
+            self.a = a
+            self.e = e
+            self.i = i
+            self.arg = arg
+            self.node = node
+            self.M0 = M0
+        
         self.epoch = epoch
         self.obs_date = obs_date
         # compute M for given epoch
-        self.M = (2.9630927492415936E-04/self.a**3)**0.5 * (self.obs_date - self.epoch) + self.M0
-        self.X, self.Y, self.Z = self.kep_to_xyz(self.a, self.e, self.i, self.arg, self.node, self.M)
+        self.M = (self.u/self.a**3)**0.5 * (self.obs_date - self.epoch) + self.M0
+        self.X, self.Y, self.Z, self.VX, self.VY, self.VZ = self.kep_to_xyz(self.a, self.e, self.i, self.arg, self.node, self.M)
         self.ra, self.dec = self.xyz_to_equa(self.X, self.Y, self.Z, self.obs_date)
  
     def cal_E(self, e, M):
@@ -52,31 +64,105 @@ class propagate:
         v = 2 * np.arctan2((1 + e)**0.5*np.sin(E/2.), (1 - e)**0.5*np.cos(E/2.))
         # compute the barycentric distance r
         r = a * (1 - e*np.cos(E))
+        # compute the specific angular momentum h
+        h = (self.u * a * (1-e**2)) **0.5
         # compute X,Y,Z
         X = r * (np.cos(node) * np.cos(arg + v) - np.sin(node) * np.sin(arg + v) * np.cos(i))
         Y = r * (np.sin(node) * np.cos(arg + v) + np.cos(node) * np.sin(arg + v) * np.cos(i))
         Z = r * (np.sin(i) * np.sin(arg + v))
-        return X, Y, Z
+        VX = (X * h * e) * np.sin(v) / (r*a*(1-e**2)) - h/r *(np.cos(node) * np.sin(arg + v) + np.sin(node) * np.cos(arg + v) * np.cos(i))
+        VY = (Y * h * e) * np.sin(v) / (r*a*(1-e**2)) - h/r *(np.sin(node) * np.sin(arg + v) - np.cos(node) * np.cos(arg + v) * np.cos(i))
+        VZ = (Z * h * e) * np.sin(v) / (r*a*(1-e**2)) + h/r * np.sin(i) * np.cos(arg + v)
+        return X, Y, Z, VX, VY, VZ
            
+    def xyz_to_kep(self, X, Y, Z, VX, VY, VZ):
+        # compute the barycentric distance r
+        r = (X**2 + Y**2 + Z**2)**0.5
+        rrdot = (X*VX + Y*VY + Z*VZ)
+        # compute the specific angular momentum h
+        hx = Y * VZ - Z * VY
+        hy = Z * VX - X * VZ
+        hz = X * VY - Y * VX
+        h = (hx**2 + hy**2 + hz**2)**0.5
+        # compute eccentricity vector
+        ex = (VY * hz - VZ * hy)/self.u - X/r
+        ey = (VZ * hx - VX * hz)/self.u - Y/r
+        ez = (VX * hy - VY * hx)/self.u - Z/r
+        e = (ex**2+ey**2+ez**2)**0.5
+        # compute vector n
+        nx = -hy
+        ny = hx
+        nz = 0
+        n = (nx**2 + ny**2)**0.5
+        # compute true anomaly v, the angle between e and r
+        v = np.arccos((ex * X + ey * Y + ez * Z) / (e*r))
+        v[rrdot<0] = 2*np.pi - v[rrdot<0]
+        # compute inclination
+        i = np.arccos(hz/h)
+        # compute eccentric anomaly E
+        E = 2*np.arctan(np.tan(v/2)/((1+e)/(1-e))**0.5)
+        # compute ascending node
+        node = np.arccos(nx/n)
+        node[ny<0] = 2*np.pi - node[ny<0]
+        # compute argument of periapsis, the angle between e and n
+        arg = np.arccos((nx * ex + ny * ey + nz *ez) / (n*e))
+        arg[ez<0] = 2*np.pi - arg[ez<0]
+        # compute mean anomaly
+        M = E - e * np.sin(E)
+        M[M<0] += 2*np.pi
+        # compute a
+        a = 1/(2/r - (VX**2+VY**2+VZ**2)/self.u)
+        return a, e, i, arg, node, M   
+        
+    def helio_to_bary(self, a, e, i, arg, node, M, epoch):
+        # This is heliocentric xyz postion
+        
+        X0, Y0, Z0, VX0, VY0, VZ0 =  self.kep_to_xyz(a, e, i, arg, node, M)
+        # extract barycentric postion of Sun
+        sun = planets['Sun']
+        ts = load.timescale()
+        t = ts.tai(jd=epoch+0.000428) #37 leap seconds
+        x_sun, y_sun, z_sun = sun.at(t).position.au
+        vx_sun, vy_sun, vz_sun = sun.at(t).velocity.au_per_d
+        #print(x_sun,y_sun * np.cos(-self.epsilon) - z_sun * np.sin(-self.epsilon),y_sun * np.sin(-self.epsilon) + z_sun * np.cos(-self.epsilon), vx_sun,vy_sun * np.cos(-self.epsilon) - vz_sun * np.sin(-self.epsilon),vy_sun * np.sin(-self.epsilon) + vz_sun * np.cos(-self.epsilon))
+        #print((x_sun**2+y_sun**2+z_sun**2)**0.5, (vx_sun**2+vy_sun**2+vz_sun**2)**0.5)
+        # now we have barycentric xyz postion 
+        X = X0 + x_sun
+        Y = Y0 + y_sun * np.cos(-self.epsilon) - z_sun * np.sin(-self.epsilon)
+        Z = Z0 + y_sun * np.sin(-self.epsilon) + z_sun * np.cos(-self.epsilon)
+        VX = VX0 + vx_sun
+        VY = VY0 + vy_sun * np.cos(-self.epsilon) - vz_sun * np.sin(-self.epsilon)
+        VZ = VZ0 + vy_sun * np.sin(-self.epsilon) + vz_sun * np.cos(-self.epsilon)
+        print(vx_sun, (vy_sun * np.cos(-self.epsilon) - vz_sun * np.sin(-self.epsilon)), ( vy_sun * np.sin(-self.epsilon) + vz_sun * np.cos(-self.epsilon)))
+        #print(VX0**2+VY0**2+VZ0**2)
+        print(X-X0,Y-Y0,Z-Z0,VX-VX0,VY-VY0,VZ-VZ0)
+        # transfer back to keplerian elements
+        a, e, i, arg, node, M = self.xyz_to_kep(X, Y, Z, VX, VY, VZ)
+        return a, e, i, arg, node, M
+
     def xyz_to_equa(self, X0, Y0, Z0, epoch):
         c = 173.1446323547978
         earth = planets['earth']
+        earth = earth + Topos('30.169 S', '70.804 W', elevation_m=2200)
         ts = load.timescale()
         t = ts.tai(jd=epoch+0.000428) #37 leap seconds
-        epsilon =  23.43929 * np.pi/180. # obliquity
         x_earth, y_earth, z_earth = earth.at(t).position.au # earth IRCS position
         earth_dis = (x_earth**2 + y_earth**2 + z_earth**2)**0.5
+<<<<<<< HEAD
         
         for i in range(3): #iterate 3 time to calibate the effect of light traveling time
+=======
+        for i in range(3): 
+>>>>>>> devel
             # transfer ecliptic to IRCS and shift to Geocentric
             X = X0 - x_earth 
-            Y = Y0 * np.cos(epsilon) - Z0 * np.sin(epsilon) - y_earth
-            Z = Y0 * np.sin(epsilon) + Z0 * np.cos(epsilon) - z_earth
+            Y = Y0 * np.cos(self.epsilon) - Z0 * np.sin(self.epsilon) - y_earth
+            Z = Y0 * np.sin(self.epsilon) + Z0 * np.cos(self.epsilon) - z_earth
             delta = (X**2 + Y**2+ Z**2)**0.5
             ltt = delta / c
-            M = (2.9630927492415936E-04/self.a**3)**0.5 * (-ltt) + self.M
-            X0, Y0, Z0 = self.kep_to_xyz(self.a, self.e, self.i, self.arg, self.node, M)
-            
+            M = (self.u/self.a**3)**0.5 * (-ltt) + self.M
+            X0, Y0, Z0, VX0, VY0, VZ0 = self.kep_to_xyz(self.a, self.e, self.i, self.arg, self.node, M)
+        #print(X,Y,Z)    
         # Cartesian to spherical coordinate
         dec = np.arcsin(Z/(X**2+Y**2+Z**2)**0.5)
         ra = np.arctan2(Y, X) % (2*np.pi)
@@ -102,9 +188,21 @@ if __name__ == '__main__':
     sedna2 = np.array([5.074246374075075E+02, 8.498746087028334E-01, 1.192855886739805E+01*np.pi/180., 3.113066085007133E+02*np.pi/180., 1.444030959000094E+02*np.pi/180., 3.581035054464383E+02*np.pi/180., 2457375.5, 2458471.5])
     eris0 = np.array([6.783405036515818E+01, 4.384475538521589E-01, 4.399285116152476E+01*np.pi/180., 1.512196225116616E+02*np.pi/180., 3.597654134974561E+01*np.pi/180., 2.041397376676136E+02*np.pi/180., 2457375.5, 2457740.5])
     eris1 = np.array([6.783405036515818E+01, 4.384475538521589E-01, 4.399285116152476E+01*np.pi/180., 1.512196225116616E+02*np.pi/180., 3.597654134974561E+01*np.pi/180., 2.041397376676136E+02*np.pi/180., 2457375.5, 2458105.5])
-    eris2 = np.array([6.783405036515818E+01, 4.384475538521589E-01, 4.399285116152476E+01*np.pi/180., 1.512196225116616E+02*np.pi/180., 3.597654134974561E+01*np.pi/180., 2.041397376676136E+02*np.pi/180., 2457375.5, 2458470.5])
-    object = np.array([sedna0, sedna1, sedna2, eris0, eris1, eris2])
+    eris2 = np.array([6.783405036515818E+01, 4.384475538521589E-01, 4.399285116152476E+01*np.pi/180., 1.512196225116616E+02*np.pi/180., 3.597654134974561E+01*np.pi/180., 2.041397376676136E+02*np.pi/180., 2457375.5, 2457375.5])
+    eris_helio = np.array([6.768797414697519E+01, 4.420790192749195E-01, 4.413609832595201E+01*np.pi/180., 1.511834587301832E+02*np.pi/180., 3.590950696773813E+01*np.pi/180., 2.044966970545773E+02*np.pi/180., 2457375.5, 2457375.5])
+    GH137 = np.array([39.439983, 0.228133, 13.468*np.pi/180., 152.542*np.pi/180., 35.741*np.pi/180., 0, 2452153.02, 2458285.54166667])
+    GH137_2 = np.array([3.943811437738326E+01, 2.280719112904044E-01, 1.346811006640707E+01*np.pi/180., 1.525597064620324E+02*np.pi/180., 3.574051307337605E+01*np.pi/180., 2.441056774137685E+01*np.pi/180., 2458285.5, 2458285.5])
+    GH137_helio = np.array([3.979291654743955E+01, 2.336699438884731E-01, 1.344241830596374E+01*np.pi/180., 1.536827090353943E+02*np.pi/180., 3.572856612008427E+01*np.pi/180., 2.338003676460184E+01*np.pi/180., 2458285.5, 2458285.5])
+    BP519 = np.array([4.490784528803734E+02, 9.215119038599956E-01, 5.411067896592579E+01*np.pi/180., 3.480604843629308E+02*np.pi/180., 1.352131890380128E+02*np.pi/180., 3.584734429420708E+02*np.pi/180., 2458285.5, 2458285.5])
+    BP519_helio = np.array([4.290994031655086E+02, 9.180967097705615E-01, 5.411891760737154E+01*np.pi/180., 3.483996318327017E+02*np.pi/180., 1.351726869671246E+02*np.pi/180., 3.583603303959885E+02*np.pi/180., 2458285.5, 2458285.5])
+    #object = np.array([sedna0, sedna1, sedna2, eris0, eris1, eris2, GH137, GH137_2, BP519, BP519_helio])
+    object = np.array([BP519])
     p = propagate(object.T[0], object.T[1], object.T[2], object.T[3], object.T[4], object.T[5], object.T[6], object.T[7])
-    print(p.ra*180/np.pi, p.dec*180/np.pi)
-
-
+    print(p.a, p.e, p.i, p.arg, p.node, p.M)
+    print(p.ra*180/np.pi, p.dec*180/np.pi, p.X, p.Y, p.Z, p.VX, p.VY, p.VZ)
+    print(p.VX**2+p.VY**2+p.VZ**2)
+    object = np.array([BP519_helio])
+    p1 = propagate(object.T[0], object.T[1], object.T[2], object.T[3], object.T[4], object.T[5], object.T[6], object.T[7], helio=True)
+    print(p1.a, p1.e, p1.i, p1.arg, p1.node, p1.M)
+    print(p1.ra*180/np.pi, p1.dec*180/np.pi, p1.X, p1.Y, p1.Z, p1.VX, p1.VY, p1.VZ)
+    print(((p1.ra-p.ra)**2+(p1.dec-p.dec)**2)**0.5*180/np.pi*3600)
