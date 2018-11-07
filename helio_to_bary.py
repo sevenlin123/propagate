a76dd08a017d2ff401c097939322ba0eee5e66bc
Edward Lin <hsingwel@umich.edu>
2018-11-07 12:47:40 -0500

    v0.2: add bary to helio transformation

--- helio_to_bary.py
+++ helio_to_bary.py
@@ -1,9 +1,11 @@
 #############################################################################
 #
-# helio_to_bary.py, version 0.1
+# helio_to_bary.py, version 0.2
 #
 # transfer heliocentric Keplerian elements to barycentric Keplerian elements    
-#  
+# 
+# v0.2: add barycentric to heliocentric transformation
+# 
 # Author: 
 # Edward Lin hsingwel@umich.edu
 #############################################################################
@@ -19,7 +21,7 @@ planets = load('de423.bsp')
 
 class transformation:
     """
-    transfer heliocentric Keplerian elements to barycentric Keplerian elements
+    convert between heliocentric Keplerian elements and barycentric Keplerian elements
     Units: au, radian, JD
       
     """
@@ -28,7 +30,12 @@ class transformation:
         self.u_helio = 2.9591220828559093E-04 # GM, M is the mass of sun
         self.epsilon =  23.43929111 * np.pi/180. # obliquity
         self.epoch = epoch
-        self.a, self.e, self.i, self.arg, self.node, self.M =  self.helio_to_bary(a,  e, i, arg, node, M0, epoch)
+        self.a0 = a
+        self.e0 = e
+        self.i0 = i
+        self.arg0 = arg
+        self.node0 = node
+        self.M0 = M0
 
     def cal_E(self, e, M):
         # compute eccentric anomaly E
@@ -99,16 +106,16 @@ class transformation:
         a = 1/(2/r - (VX**2+VY**2+VZ**2)/u)
         return a, e, i, arg, node, M   
         
-    def helio_to_bary(self, a0, e0, i0, arg0, node0, M0, epoch0):
+    def helio_to_bary(self):
         # This is heliocentric xyz postion       
-        X0, Y0, Z0, VX0, VY0, VZ0 =  self.kep_to_xyz(a0, e0, i0, arg0, node0, M0, self.u_helio)
+        X0, Y0, Z0, VX0, VY0, VZ0 =  self.kep_to_xyz(self.a0, self.e0, self.i0, self.arg0, self.node0, self.M0, self.u_helio)
         # extract barycentric postion of Sun
         sun = planets['Sun']
         ts = load.timescale()
-        t = ts.tai(jd=epoch0+0.000428) #37 leap seconds
+        t = ts.tai(jd=self.epoch+0.000428) #37 leap seconds
         x_sun, y_sun, z_sun = sun.at(t).position.au
         vx_sun, vy_sun, vz_sun = sun.at(t).velocity.au_per_d
-        # now we have barycentric xyz postion 
+        # now calculate the heliocentric xyz postions of objects
         X = X0 + x_sun
         Y = Y0 + y_sun * np.cos(-self.epsilon) - z_sun * np.sin(-self.epsilon)
         Z = Z0 + y_sun * np.sin(-self.epsilon) + z_sun * np.cos(-self.epsilon)
@@ -116,19 +123,56 @@ class transformation:
         VY = VY0 + vy_sun * np.cos(-self.epsilon) - vz_sun * np.sin(-self.epsilon)
         VZ = VZ0 + vy_sun * np.sin(-self.epsilon) + vz_sun * np.cos(-self.epsilon)
         # transfer back to keplerian elements
-        a, e, i, arg, node, M = self.xyz_to_kep(X, Y, Z, VX, VY, VZ, self.u_bary)
-        return a, e, i, arg, node, M
+        self.a, self.e, self.i, self.arg, self.node, self.M = self.xyz_to_kep(X, Y, Z, VX, VY, VZ, self.u_bary)
+
+    def bary_to_helio(self):
+        # This is barycentric xyz postion       
+        X0, Y0, Z0, VX0, VY0, VZ0 =  self.kep_to_xyz(self.a0, self.e0, self.i0, self.arg0, self.node0, self.M0, self.u_bary)
+        # extract barycentric postion of Sun
+        sun = planets['Sun']
+        ts = load.timescale()
+        t = ts.tai(jd=self.epoch+0.000428) #37 leap seconds
+        x_sun, y_sun, z_sun = sun.at(t).position.au
+        vx_sun, vy_sun, vz_sun = sun.at(t).velocity.au_per_d
+        # now calculate the heliocentric xyz postions of objects
+        X = X0 - x_sun
+        Y = Y0 - y_sun * np.cos(-self.epsilon) + z_sun * np.sin(-self.epsilon)
+        Z = Z0 - y_sun * np.sin(-self.epsilon) - z_sun * np.cos(-self.epsilon)
+        VX = VX0 - vx_sun
+        VY = VY0 - vy_sun * np.cos(-self.epsilon) + vz_sun * np.sin(-self.epsilon)
+        VZ = VZ0 - vy_sun * np.sin(-self.epsilon) - vz_sun * np.cos(-self.epsilon)
+        # transfer back to keplerian elements
+        self.a, self.e, self.i, self.arg, self.node, self.M = self.xyz_to_kep(X, Y, Z, VX, VY, VZ, self.u_helio)
+
 
 def helio_to_bary(list_of_object):
         object = np.array(list_of_object)
-        # the transformation class takes radians, please turn degrees to radians
+        # the transformation class takes radians, turn elements to radians
         h2b = transformation(object.T[0], object.T[1], object.T[2]*np.pi/180., object.T[3]*np.pi/180., object.T[4]*np.pi/180., object.T[5]*np.pi/180., object.T[6])
-        elements = np.array([h2b.a, h2b.e, h2b.i, h2b.arg, h2b.node, h2b.M, h2b.epoch]).T
+        h2b.helio_to_bary()
+        # now turn the radians back to degrees
+        elements = np.array([h2b.a, h2b.e, h2b.i*180/np.pi, h2b.arg*180/np.pi, h2b.node*180/np.pi, h2b.M*180/np.pi, h2b.epoch]).T
         return elements
 
+def bary_to_helio(list_of_object):
+        object = np.array(list_of_object)
+        # the transformation class takes radians, turn elements to radians
+        b2h = transformation(object.T[0], object.T[1], object.T[2]*np.pi/180., object.T[3]*np.pi/180., object.T[4]*np.pi/180., object.T[5]*np.pi/180., object.T[6])
+        b2h.bary_to_helio()
+        # now turn the radians back to degrees
+        elements = np.array([b2h.a, b2h.e, b2h.i*180/np.pi, b2h.arg*180/np.pi, b2h.node*180/np.pi, b2h.M*180/np.pi, b2h.epoch]).T
+        return elements
+
+
 if __name__ == '__main__':
-        eris_helio = np.array([6.768797414697519E+01, 4.420790192749195E-01, 4.413609832595201E+01, 1.511834587301832E+02, 3.590950696773813E+01, 2.044966970545773E+02, 2457375.5])
-        GH137_helio = np.array([3.979291654743955E+01, 2.336699438884731E-01, 1.344241830596374E+01, 1.536827090353943E+02, 3.572856612008427E+01, 2.338003676460184E+01, 2458285.5])
-        bp_helio = np.array([420.48310521, 0.91645907, 0.9445533*180/np.pi, 6.08211338*180/np.pi, 2.35920844*180/np.pi, 6.25364812*180/np.pi, 2458285.5])
-        bary = helio_to_bary([eris_helio, GH137_helio, bp_helio])
+        eris_helio = np.array([6.768806773065693E+01, 4.420836167285916E-01, 4.413632128654130E+01, 1.511803890073615E+02, 3.590938986466752E+01, 2.045040412784239E+02, 2457375.5])
+        ruby_helio = np.array([3.004664043215652E+01, 8.282679611502294E-02, 3.130856167451365E+01, 2.165133203133375E+02, 1.925864845971902E+02, 3.517576218761481E+02, 2457375.5])
+        bp_helio = np.array([4.532976896627495E+02, 9.224097494635715E-01, 5.410743429007145E+01, 3.481369148319624E+02, 1.353379395756713E+02, 3.584014239710087E+02, 2457375.5])
+        eris_bary = np.array([6.78341374e+01, 4.38452144e-01, 4.39930733e+01, 1.51216518e+02, 3.59764240e+01, 2.04147112e+02, 2.45737550e+06])
+        ruby_bary = np.array([3.00866683e+01, 8.37151911e-02, 3.12584148e+01, 2.15447926e+02, 1.92538236e+02, 3.52704806e+02, 2.45737550e+06])
+        bp_bary = np.array([4.49085364e+02, 9.21513173e-01, 5.41106748e+01, 3.48060483e+02, 1.35213249e+02, 3.58379172e+02, 2.45737550e+06])
+        
+        bary = helio_to_bary([eris_helio, ruby_helio, bp_helio])
+        helio = bary_to_helio([eris_bary, ruby_bary, bp_bary])
         print(bary)
+        print(helio)
